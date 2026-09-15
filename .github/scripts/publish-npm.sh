76921601cb99057814da2341ffc3072c7a2c02e1
Liz Ainslie <lizzy@lizainslie.dev>
2026-09-15 20:43:05 +0000

    fix: npm dual-publish auth lines

--- .github/scripts/publish-npm.sh
+++ .github/scripts/publish-npm.sh
@@ -9,7 +9,7 @@ PACKAGES=(@kolektiv/themes)
 publish_one_registry() {
   local name="$1"
   local registry="$2"
-  local auth_line="$3"
+  local auth_userconfig_lines="$3"
 
   local NPMRC_PATH="${ROOT}/.npmrc"
   local backup
@@ -18,14 +18,9 @@ publish_one_registry() {
   restore() { cp "${backup}" "${NPMRC_PATH}"; rm -f "${backup}"; }
   trap restore EXIT
 
-  local path="${registry#https://}"
-  path="${path#http://}"
-  path="${path%/}/"
-
   {
-    printf '\n@kolektiv:registry=%s\n' "${registry}"
-    printf '%s\n' "${auth_line}"
-    printf '//%salways-auth=true\n' "${path}"
+    printf '\n'
+    printf '%s\n' "${auth_userconfig_lines}"
   } >>"${NPMRC_PATH}"
 
   already_published() {
@@ -45,19 +40,18 @@ publish_one_registry() {
   trap - EXIT
 }
 
-# 1) Yuri Capital (required secrets)
 : "${YURI_CAPITAL_REPO_USERNAME:?set YURI_CAPITAL_REPO_USERNAME}"
 : "${YURI_CAPITAL_REPO_PASSWORD:?set YURI_CAPITAL_REPO_PASSWORD}"
 YURI_REGISTRY="https://repo.yuri.capital/repository/brand-npm/"
 YURI_AUTH="$(printf '%s:%s' "${YURI_CAPITAL_REPO_USERNAME}" "${YURI_CAPITAL_REPO_PASSWORD}" | openssl base64 -A)"
 YURI_PATH="${YURI_REGISTRY#https://}"
-publish_one_registry "yuri-capital" "${YURI_REGISTRY}" "//${YURI_PATH}:_auth=${YURI_AUTH}"
+YURI_PATH="${YURI_PATH#http://}"
+publish_one_registry "yuri-capital" "${YURI_REGISTRY}" "$(printf '@kolektiv:registry=%s\n//%s:_auth=%s\n//%s:always-auth=true' "${YURI_REGISTRY}" "${YURI_PATH}" "${YURI_AUTH}" "${YURI_PATH}")"
 
-# 2) GitHub Packages (when GITHUB_TOKEN present)
 if [ -n "${GITHUB_TOKEN:-}" ] || [ -n "${NODE_AUTH_TOKEN:-}" ]; then
   TOKEN="${NODE_AUTH_TOKEN:-${GITHUB_TOKEN}}"
   GH_REGISTRY="https://npm.pkg.github.com/"
-  publish_one_registry "github-packages" "${GH_REGISTRY}" "//npm.pkg.github.com/:_authToken=${TOKEN}"
+  publish_one_registry "github-packages" "${GH_REGISTRY}" "$(printf '@kolektiv:registry=%s\n//npm.pkg.github.com/:_authToken=%s\n//npm.pkg.github.com/:always-auth=true' "${GH_REGISTRY}" "${TOKEN}")"
 else
   echo "::warning::GITHUB_TOKEN/NODE_AUTH_TOKEN unset; skipping npm GitHub Packages publish"
 fi
